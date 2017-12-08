d91b4baecbf67593156e179388f5d4c0a1e69dd7
Kyle <k90armstrong@gmail.com>
2017-12-08 00:14:28 -0700

    finishes the apps main functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,7 +28,7 @@ function startApp() {
         var choices = [];
         for (var i = 0; i < res.length; i++) {
             var choice = {
-                name: res[i].product_name,
+                name: res[i].product_name + " $" + res[i].customer_price,
                 value: res[i].item_id
             }
             choices.push(choice);
@@ -55,9 +55,60 @@ function startApp() {
             ]).then(function(answer) {
                 var quantity = answer.quantity;
                 var productId = answer.product;
-                console.log('You want to buy ' + quantity + ' ' + productId);
+                processRequest({id: productId, qty: quantity}, function(err) {
+                    if (err) {
+                        console.log('There was an error', err);
+                    } else {
+                        askIfFinished(function(response) {
+                            if (response === 'no') {
+                                console.log('Thanks!');
+                                process.exit();
+                            } else {
+                                startApp();
+                            }
+                        })
+                    }
+                });
             });
     
+    });
+}
+
+function processRequest(data, cb) {
+    var query1 = "SELECT stock_quantity FROM products WHERE ?";
+    var query2 = "UPDATE products SET ?"
+    connection.query(query1, {item_id: data.id}, function(err, res) {
+        if (err){
+            return cb(err);
+        }
+        if (res[0].stock_quantity >= data.qty) {
+            // we can do the purchase
+            var newQty = {stock_quantity: res[0].stock_quantity - data.qty};
+            connection.query(query2, newQty, function(err, res) {
+                if (err) {
+                    return cb(err);
+                } 
+                console.log('Purchase Successful!');
+                return cb();
+            });
+        } else {
+            // we can't do the purchase, there aren't enough
+            console.log('There are only ' + res[0].stock_quantity + ' items remaing!');
+            return cb();
+        }
+    });
+
+}
+
+function askIfFinished(cb) {
+    inquirer
+    .prompt([{
+        name: "answer",
+        type: "list",
+        message: "Would you like to buy another item?",
+        choices: ["yes", "no"]
+    }]).then(function(answer){
+        return cb(answer.answer);
     });
 }
 
